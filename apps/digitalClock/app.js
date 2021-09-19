08b95ae8c7bb466463d26706bef9699c462e8c39
bibashmgr <bibashthapamagar21@gmail.com>
2021-09-19 22:42:32 +0545

    Project 03

---
+++ apps/digitalClock/app.js
@@ -0,0 +1,58 @@
+// variable declaration:
+const hours = document.getElementById("hours-span");
+const minutes = document.getElementById("mins-span");
+const seconds = document.getElementById("secs-span");
+
+// clock:
+// let currentTime = () => {
+//     let currentDate = new Date();
+//     let hrs = currentDate.getHours();
+//     let mins = currentDate.getMinutes();
+//     let secs = currentDate.getSeconds();
+
+//     if (hrs < 10) {
+//         hrs = "0" + hrs;
+//     }
+//     if (mins < 10) {
+//         mins = "0" + mins;
+//     }
+//     if (secs < 10) {
+//         secs = "0" + secs;
+//     }
+
+//     hours.innerText = hrs;
+//     minutes.innerText = mins;
+//     seconds.innerText = secs;
+
+//     setTimeout("currentTime()", 1000);
+// };
+let currentTime = () => {
+    let currentDate = new Date();
+    let hrs = currentDate.getHours();
+    let mins = currentDate.getMinutes();
+    let secs = currentDate.getSeconds();
+
+    if (hrs < 10) {
+        hrs = "0" + hrs;
+    }
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
+
+    hours.innerText = hrs;
+    minutes.innerText = mins;
+    seconds.innerText = secs;
+
+    setTimeout("currentTime()", 1000);
+};
+
+currentTime();
+
+// dark-mode
+const darkMode = (boxName) => {
+    boxName.classList.toggle("active");
+    document.body.classList.toggle("active");
+};
---
+++ apps/digitalClock/readme.md
@@ -0,0 +1,7 @@
+## Digital clock
+
+A web app that shows the current time in digital format.
+
+__Features:__
+- shows current time
+- dark mode

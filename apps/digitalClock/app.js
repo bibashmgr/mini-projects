bd437df8a714105bf736583e1cda1d5439e8ca58
bibashmgr <bibashthapamagar21@gmail.com>
2021-09-20 21:17:08 +0545

    Project 03

--- apps/digitalClock/app.js
+++ apps/digitalClock/app.js
@@ -3,29 +3,6 @@ const hours = document.getElementById("hours-span");
 const minutes = document.getElementById("mins-span");
 const seconds = document.getElementById("secs-span");
 
-// clock:
-// let currentTime = () => {
-//     let currentDate = new Date();
-//     let hrs = currentDate.getHours();
-//     let mins = currentDate.getMinutes();
-//     let secs = currentDate.getSeconds();
-
-//     if (hrs < 10) {
-//         hrs = "0" + hrs;
-//     }
-//     if (mins < 10) {
-//         mins = "0" + mins;
-//     }
-//     if (secs < 10) {
-//         secs = "0" + secs;
-//     }
-
-//     hours.innerText = hrs;
-//     minutes.innerText = mins;
-//     seconds.innerText = secs;
-
-//     setTimeout("currentTime()", 1000);
-// };
 let currentTime = () => {
     let currentDate = new Date();
     let hrs = currentDate.getHours();
